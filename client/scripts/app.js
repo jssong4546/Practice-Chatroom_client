84d50dfbcc1974a1d6918322b3f6c1693ad2855a
aiody <aiody329@gmail.com>
2020-04-07 15:49:34 +0900

    complete making a room

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,17 +9,18 @@ const app = {
     fetch(app.server)
       .then((res) => res.json())
       .then((res) => {
-        this.messages = JSON.parse(JSON.stringify(res));
-        res.forEach((element) => {
-          this.renderMessage(element);
-        });
-        this.showRooms();
+        app.messages = JSON.parse(JSON.stringify(res));
+        app.makeRoomList();
+        app.renderFilteredMessages();
+        //setTimeout(app.fetch, 1000);
       });
   },
   messages: [],
   clearMessages: function () {
     let chats = document.querySelector('#chats');
     chats.innerHTML = '';
+    document.querySelector('#username').value = '';
+    document.querySelector('#message').value = '';
   },
   renderMessage: function (msg) {
     let comment = document.createElement('div');
@@ -47,47 +48,55 @@ const app = {
     }).then(response => {
       return response.json();
     }).then(json => {
-      console.log(json);
-      // message sent!
+      app.clearMessages();
+      app.messages.push(msg);
+      app.renderFilteredMessages(msg.roomname);
     });
   },
-  showRooms: function () {
+  makeRoomList: function () {
     let allRoom = this.messages.map((ele) => {
       return ele.roomname;
     });
     let roomWithoutRepeat = Array.from(new Set(allRoom));
 
+    let rooms = document.querySelector('#rooms');
+    rooms.innerHTML = '';
     roomWithoutRepeat.forEach((ele) => {
       let room = document.createElement('option');
       room.className = 'room';
       room.value = ele;
       room.innerHTML = ele;
-      let rooms = document.querySelector('#rooms');
-      rooms.addEventListener('change', selectRoom);
+      rooms.addEventListener('change', app.selectRoom);
       rooms.appendChild(room);
     });
   },
+  selectRoom: function (event) {
+    app.renderFilteredMessages(event.target.value);
+  },
+  renderFilteredMessages: function (room = '') {
+    let rooms = document.querySelector('#rooms');
+    let selected = room === '' ? rooms.options[rooms.selectedIndex].value : room;
+    let filtered = app.messages.filter((ele) => ele.roomname === selected);
+
+    let roomName = document.querySelector('#roomname');
+    roomName.value = selected;
+    
+    app.clearMessages();
+    filtered.forEach(ele => app.renderMessage(ele));
+  }
 };
 
 app.init();
 
 function inputData() {
-  const message = {
-    roomname: 'cs04',
-  };
+  const message = {};
   let nameInput = document.querySelector('#username');
   let messageInput = document.querySelector('#message');
+  let roomInput = document.querySelector('#roomname');
 
   message.username = nameInput.value;
   message.text = messageInput.value;
+  message.roomname = roomInput.value;
 
   app.send(message);
-}
-
-function selectRoom() {
-  let rooms = document.querySelector('#rooms');
-  let selected = rooms.options[rooms.selectedIndex].value;
-  let filtered = app.messages.filter((ele) => ele.roomname === selected);
-  app.clearMessages();
-  filtered.forEach(ele => app.renderMessage(ele));
 }
